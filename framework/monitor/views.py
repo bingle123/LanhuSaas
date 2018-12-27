1dbd2dda37ecb167059444ae0b9e09dd5e4f9799
cy18390810647 <1013788064@qq.com>
2018-12-27 16:07:50 +0800

    cy12.27

--- framework/monitor/views.py
+++ framework/monitor/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+import function
+from common.mymako import render_json, render_mako_context
+
 
 # Create your views here.
+
+def index(request):
+
+    return render_mako_context(request, 'show_message.html')
+
+
+def unit_show(request):
+
+    res = function.unit_show(request)
+    return render_json(res)
+
+
+def edit_unit(request):
+
+    res = function.edit_unit(request)
+    return render_json(res)
+
