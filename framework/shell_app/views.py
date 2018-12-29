a416a8732571fbb8c31071914dd2796d10274083
DeleMing <761494073@qq.com>
2018-12-29 15:25:46 +0800

    2018/12/28 15:25 liaomingtao ----- vue——elementUI学习，基本能用

--- framework/shell_app/views.py
+++ framework/shell_app/views.py
@@ -4,6 +4,25 @@ from common.mymako import render_mako_context
 from shell_app import function
 import time
 
+
+def add_scene(request):
+    """
+    增加场景表单HTML
+    :param request:
+    :return:
+    """
+    return render_mako_context(request, './shell_app/add_scene.html')
+
+
+def scene_list(request):
+    """
+    场景管理列表
+    :param request:
+    :return:
+    """
+    return render_mako_context(request, './shell_app/scene_list.html')
+
+
 def carousel(request):
     """
     页面轮播效果
@@ -89,6 +108,17 @@ def get_positions_all(request):
     """
     result = function.get_positions_all()
     return render_json(result)
+
+
+def add_scene_form(request):
+    """
+    增加场景表单
+    :param request:
+    :return:
+    """
+    print 111
+    print request.body
+    return render_json(function.get_scenes_all())
 
 
 def get_json_test(request):
