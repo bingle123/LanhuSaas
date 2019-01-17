dfdc54d00895fbf556aec01d5d58a41352dfbf30
cy18390810647 <1013788064@qq.com>
2019-01-17 18:08:00 +0800

    数据量表重建

--- framework/monitor/function.py
+++ framework/monitor/function.py
@@ -1,16 +1,29 @@
 # -*- coding: utf-8 -*-
 from django.views.decorators.csrf import csrf_exempt
 import json
-from models import unit_administration
+from models import JobUnit,ChartUnit,Common,BasicUnit,FlowUnit
 import tools
 from django.forms.models import model_to_dict
 
 
 def unit_show(request):
-    unit = unit_administration.objects.all()
+    unit = Common.objects.all()
     res_list = []
     for i in unit:
-        dic = model_to_dict(i)
+        dic = {
+            'id': i.id,
+            'unit_name': i.unit_name,
+            'unit_type': i.unit_type,
+            'editor': i.editor,
+            'edit_time': str(i.edit_time),
+            'font_size': i.font_size,
+            'height': i.height,
+            'wide': i.wide,
+            'switch': i.switch,
+            'start_time': str(i.start_time),
+            'end_time': str(i.end_time),
+            'cycle': i.cycle,
+        }
         res_list.append(dic)
     return res_list
 
@@ -19,27 +32,27 @@ def select_unit(request):
     try:
         res = request.body
         res_list = []
-        unit_list=[]
+        unit_list = []
         res1 = "{}".format(res)
         if len(res1) == 0:
             res_list = unit_show(request)
         else:
             if res1.isdigit():
-                if unit_administration.objects.filter(id=int(res1)).exists():
-                    unit = unit_administration.objects.filter(id=int(res1))
+                if Common.objects.filter(id=int(res1)).exists():
+                    unit = Common.objects.filter(id=int(res1))
                     unit_list.append(unit)
-            if unit_administration.objects.filter(unit_name=res1).exists():
-                unit = unit_administration.objects.filter(unit_name=res1)
+            if Common.objects.filter(unit_name=res1).exists():
+                unit = Common.objects.filter(unit_name=res1)
                 unit_list.append (unit)
-            if unit_administration.objects.filter(unit_type=res1).exists():
-                unit = unit_administration.objects.filter(unit_type=res1)
+            if Common.objects.filter(unit_type=res1).exists():
+                unit = Common.objects.filter(unit_type=res1)
                 unit_list.append (unit)
-            if unit_administration.objects.filter(editor=res1).exists():
-                unit = unit_administration.objects.filter(editor=res1)
+            if Common.objects.filter(editor=res1).exists():
+                unit = Common.objects.filter(editor=res1)
                 unit_list.append (unit)
-            if unit_administration.objects.filter(edit_time=res1).exists():
-                unit = unit_administration.objects.filter(edit_time=res1)
-                unit_list.append (unit)
+            if Common.objects.filter(edit_time=res1).exists():
+                unit = Common.objects.filter(edit_time=res1)
+                unit_list.append(unit)
             for i in unit_list:
                 dic = {
                     'id': i.id,
@@ -65,38 +78,38 @@ def select_unit(request):
         return None
 
 
-def delete_unit(request):
-
-    try:
-        res = request.body
-        res1 = json.loads(res)
-        unit_id = res1['unit_id']
-        unit_administration.objects.filter(id=unit_id).delete()
-
-    except Exception as e:
-        res2 = tools.error_result(e)
-        return res2
-
-@csrf_exempt
-def edit_unit(request):
-
-    try:
-        res =request.body
-        res1 = json.loads(res)
-        unit_id = res1['unit_id']
-        font_size1 = res1['font_size']
-        hight1 = res1['height']
-        wide1 = res1['width']
-        unit_name1=res1['unit_name']
-        unit_administration.objects.filter(id=unit_id).update(font_size=font_size1)
-        unit_administration.objects.filter(id=unit_id).update(hight=hight1)
-        unit_administration.objects.filter(id=unit_id).update(wide=wide1)
-        unit_administration.objects.filter(id=unit_id).update(unit_name=unit_name1)
-        return None
-    except Exception as e:
-
-        res2 = tools.error_result(e)
-        return res2
+# def delete_unit(request):
+#
+#     try:
+#         res = request.body
+#         res1 = json.loads(res)
+#         unit_id = res1['unit_id']
+#         unit_administration.objects.filter(id=unit_id).delete()
+#
+#     except Exception as e:
+#         res2 = tools.error_result(e)
+#         return res2
+#
+# @csrf_exempt
+# def edit_unit(request):
+#
+#     try:
+#         res =request.body
+#         res1 = json.loads(res)
+#         unit_id = res1['unit_id']
+#         font_size1 = res1['font_size']
+#         hight1 = res1['height']
+#         wide1 = res1['width']
+#         unit_name1=res1['unit_name']
+#         unit_administration.objects.filter(id=unit_id).update(font_size=font_size1)
+#         unit_administration.objects.filter(id=unit_id).update(hight=hight1)
+#         unit_administration.objects.filter(id=unit_id).update(wide=wide1)
+#         unit_administration.objects.filter(id=unit_id).update(unit_name=unit_name1)
+#         return None
+#     except Exception as e:
+#
+#         res2 = tools.error_result(e)
+#         return res2
 
 
 
--- framework/monitor/models.py
+++ framework/monitor/models.py
@@ -1,26 +1,60 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
-class unit_administration(models.Model):
+
+class Common(models.Model):
     """
     单元信息表
     """
-    unit_name = models.CharField(verbose_name=u'单元名称', max_length=200, unique=True)
-    unit_type = models.CharField(verbose_name=u'单元类型', max_length=200)
-    editor = models.CharField(verbose_name=u'编辑人', max_length=200)
-    edit_time = models.CharField(verbose_name=u'修改时间', max_length=200)
+    unit_name = models.CharField(verbose_name=u'单元名称', max_length=40, unique=True)
+    unit_type = models.CharField(verbose_name=u'单元类型', max_length=20)
+    editor = models.CharField(verbose_name=u'编辑人', max_length=20)
+    edit_time = models.DateTimeField(verbose_name=u'修改时间')
     font_size = models.PositiveIntegerField(verbose_name=u'字号')
-    hight = models.PositiveIntegerField(verbose_name=u'高')
+    height = models.PositiveIntegerField(verbose_name=u'高')
     wide = models.PositiveIntegerField(verbose_name=u'宽')
-    content = models.CharField(verbose_name=u'显示内容', max_length=2000)
-    data_source = models.CharField(verbose_name=u'数据来源', max_length=200)
-    time_slot = models.CharField(verbose_name=u'时间段', max_length=200)
-    time_interval = models.PositiveIntegerField(verbose_name=u'间隔时间秒')
-    template = models.CharField(verbose_name=u'数据库和模板', max_length=200)
-    chart_type = models.CharField(verbose_name=u'图表类型', max_length=200)
-    parameter = models.CharField(verbose_name=u'文件内容、脚本参数、模板参数', max_length=5000)
+    switch = models.CharField(verbose_name=u'开关', max_length=10)
+    start_time = models.DateTimeField(verbose_name=u'开始时间')
+    end_time = models.DateTimeField(verbose_name=u'结束时间')
+    cycle = models.PositiveIntegerField(verbose_name=u'采集周期')
 
 
-    class Meta:
-        verbose_name = u'单元信息'
-        verbose_name_plural = u'单元信息'
+class BasicUnit(models.Model):
+    """
+    基本单元表
+    """
+    unit_id = models.PositiveIntegerField(verbose_name=u'单元id', primary_key=True)
+    contents = models.CharField(verbose_name=u'显示内容', max_length=100)
+    sql_file_interface = models.CharField(verbose_name=u'数据来源', max_length=20)
+    sql = models.CharField(verbose_name=u'连接数据库', max_length=20)
+    rules = models.CharField(verbose_name=u'采集规则', max_length=100)
+    server = models.CharField(verbose_name=u'服务器', max_length=20)
+    file = models.CharField(verbose_name=u'文件路径', max_length=50)
+    urls = models.CharField(verbose_name=u'接口url', max_length=100)
+    param = models.CharField(verbose_name=u'接口参数', max_length=20)
+
+
+class ChartUnit(models.Model):
+    """
+    图表单元
+    """
+    unit_id = models.PositiveIntegerField(verbose_name=u'单元id', primary_key=True)
+    contents = models.CharField(verbose_name=u'显示内容', max_length=100)
+    chart_type = models.CharField(verbose_name=u'图表类型', max_length=20)
+    sql = models.CharField(verbose_name=u'连接数据库', max_length=20)
+    rules = models.CharField(verbose_name=u'采集规则', max_length=100)
+
+
+class JobUnit(models.Model):
+    unit_id = models.PositiveIntegerField(verbose_name=u'单元id', primary_key=True)
+    contents = models.CharField(verbose_name=u'显示内容', max_length=100)
+    job_mould = models.CharField(verbose_name=u'作业模板', max_length=50)
+    NODE_KEY = models.CharField(verbose_name=u'NODE_KEY', max_length=50)
+    server = models.CharField(verbose_name=u'执行服务器', max_length=20)
+
+
+class FlowUnit(models.Model):
+    unit_id = models.PositiveIntegerField(verbose_name=u'单元id', primary_key=True)
+    flow_mould = models.CharField(verbose_name=u'流程模板', max_length=50)
+    param = models.CharField(verbose_name=u'模板参数', max_length=20)
+    node = models.CharField(verbose_name=u'节点名称', max_length=20)
