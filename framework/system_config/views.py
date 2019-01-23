33c59a7df613764cf8850969517061107f0dd9b9
DeleMing <761494073@qq.com>
2019-01-23 18:27:47 +0800

    2019/1/23 18:36 --liaomingtao-- 集成爬虫，增加系统配置模块，以后的系统配置功能，都在system_config模块里写

--- framework/system_config/views.py
+++ framework/system_config/views.py
@@ -1,5 +1,8 @@
 # encoding:utf-8
 from common.mymako import render_mako_context
+from common.mymako import render_json
+import json
+import crawl_template
 
 
 # Create your views here.
@@ -11,3 +14,52 @@ def crawl_config_html(request):
     """
     return render_mako_context(request, './system_config/crawl_config.html')
 
+
+def manage_crawl(request):
+    """
+    爬虫信息新增和修改
+    :param request:
+    :return:
+    """
+    request_body = json.loads(request.body)
+    print type(request_body)
+    print request.body
+    return render_json(None)
+
+
+def delete_crawl(request):
+    """
+    根据ID删除爬虫配置信息
+    :param request:
+    :return:
+    """
+    request_body = json.loads(request.body)
+    print request_body
+    return render_json(None)
+
+
+def get_crawls(request):
+    """
+    获取爬虫配置信息
+    :param request:
+    :return:
+    """
+    request_body = json.loads(request.body)
+    print request_body
+    return render_json(None)
+
+
+def crawl(request):
+    """
+    爬虫测试
+    :param request:
+    :return:
+    """
+
+    url = 'http://www.sse.com.cn/services/tradingservice/tradingtech/newnotice/'
+    total_xpath = '//div[@class="sse_list_1 js_listPage"]//dl//dd'
+    title_xpath = 'a/text()'
+    time_xpath = 'span/text()'
+    url_xpath = 'a/@href'
+    res = crawl_template.crawl_temp(url, total_xpath, time_xpath, title_xpath, url_xpath)
+    return render_json(res)
