87e982127e83c574505ea2a7b3c235d7d39daff5
zhanglibing <zhanglibing@zork.com.cn>
2019-01-24 16:30:29 +0800

    修改表

--- framework/jobManagement/models.py
+++ framework/jobManagement/models.py
@@ -7,12 +7,13 @@ class JobInstance(models.Model):
     """
         岗位信息表
     """
-    Job_name = models.CharField(verbose_name=u'岗位名称', max_length=20,)
-    User_name = models.CharField(verbose_name=u'员工姓名', max_length=20)
-    Start_Time = models.DateField(verbose_name=u'创建时间', auto_now=True)
-    Status = models.CharField(verbose_name=u'状态', max_length=20)
-    log = models.CharField(verbose_name=u'日志', max_length=50)
+    job_name = models.CharField(verbose_name=u'岗位名称', max_length=50,)
+    create_Time = models.DateTimeField(verbose_name=u'创建时间', auto_now_add=True)
+    create_person = models.CharField(verbose_name=u'创建人', max_length=50,)
+    edit_time = models.DateTimeField(verbose_name=u'修改时间', auto_now_add=True)
+    create_person = models.CharField(verbose_name=u'修改人', max_length=50, )
 
     class Meta:
         verbose_name = u'岗位信息表'
         verbose_name_plural = u'单元信息'
+        db_table = 'tb_job_basic_info'
