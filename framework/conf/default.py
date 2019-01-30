9d5eaca7ffb09e1fa964e926d85fa86e2d9e830d
git <2221923603@qq.com>
2019-01-30 17:34:31 +0800

    新增了customProcess app

--- framework/conf/default.py
+++ framework/conf/default.py
@@ -138,6 +138,7 @@ INSTALLED_APPS = (
     'jobManagement',
     'market_day',
     'DataBaseManage',
+    'customProcess',
 )
 
 # ==============================================================================
