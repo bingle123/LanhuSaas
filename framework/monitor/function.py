b84aefee315dfa9ada9c6e61828f14c42265a688
cy18390810647 <1013788064@qq.com>
2019-02-13 10:22:31 +0800

    111

--- framework/monitor/function.py
+++ framework/monitor/function.py
@@ -6,106 +6,79 @@ import math
 from models import *
 from monitorScene.models import Scene
 import tools
+from django.core.paginator import Paginator
+from django.forms.models import model_to_dict
+from django.db.models import Q
 import copy
 
 
 def unit_show(request):
-    try:
-        res = json.loads(request.body)
-        limit = res['limit']
-        page = res['page']
-        start_page = limit*page-9
-        unit = Monitor.objects.all()[start_page-1:start_page+9]
-        unit2 = Monitor.objects.all().values('id')
-        page_count = math.ceil(len(unit2)/10)
-        res_list = []
-        for i in unit:
-            dic = {
-                'id': i.id,
-                'monitor_name': i.monitor_name,
-                'monitor_type': i.monitor_type,
-                'editor': i.editor,
-                'font_size': i.font_size,
-                'height': i.height,
-                'width': i.width,
-                'status': i.status,
-                'edit_time': str(i.edit_time),
-                'start_time': str(i.start_time),
-                'end_time': str(i.end_time),
-                'period': i.period,
-                'page_count': page_count,
-            }
-            res_list.append(dic)
-        param = {
-            'bk_username': 'admin',
-            "bk_biz_id": 2,
+
+    res = json.loads(request.body)
+    limit = res['limit']
+    page = res['page']
+    unit = Monitor.objects.all()
+    p=Paginator(unit, limit)    #分页
+    page_count = p.page_range[-1]  #总页数
+    page = p.page(page)        #当前页数据
+    res_list=[]
+    for i in page.object_list:
+        j=model_to_dict(i)
+        j['page_count']=page_count
+        j['edit_time'] = str(i.edit_time)
+        j['create_time'] = str(i.create_time)
+        j['start_time'] = str (i.start_time)
+        j['end_time'] = str (i.end_time)
+        res_list.append(j)
+    param = {
+        'bk_username': 'admin',
+        "bk_biz_id": 2,
+    }
+    client = tools.interface_param (request)
+    res = client.job.get_job_list(param)
+    if res.get('result'):
+        job_list = res.get('data')
+    else:
+        job_list = []
+        logger.error (u"请求作业模板失败：%s" % res.get ('message'))
+    job = []
+    for i in job_list:
+        dic1 = {
+            'name':i['name']
         }
-        client = tools.interface_param (request)
-        res = client.job.get_job_list(param)
-        if res.get('result'):
-            job_list = res.get('data')
-        else:
-            job_list = []
-            logger.error (u"请求作业模板失败：%s" % res.get ('message'))
-        job = []
-        for i in job_list:
-            dic1 = {
-                'name':i['name']
-            }
-            job.append(dic1)
-        res_dic = {
-            'res_list': res_list,
-            'job': job
-        }
-        result = tools.success_result(res_dic)
-    except Exception as e:
-        result = tools.error_result(e)
+        job.append(dic1)
+    res_dic = {
+        'res_list': res_list,
+        'job': job
+    }
+    result = tools.success_result(res_dic)
+    # except Exception as e:
+    #     result = tools.error_result(e)
     return result
 
 
 def select_unit(request):
-    try:
-        res = json.loads(request.body)
-        res_list = []
-        res1 = "{}".format(res['data'])
-        limit = res['limit']
-        page = res['page']
-        start_page = limit * page - 9
-        if res1.isdigit():
-            if Monitor.objects.filter(id=int(res1)).exists():
-                unit = Monitor.objects.filter(id=int(res1))[start_page-1:start_page+9]
-                unit2 = Monitor.objects.filter (id=int (res1))
-        if Monitor.objects.filter(monitor_name=res1).exists():
-            unit = Monitor.objects.filter(monitor_name=res1)[start_page-1:start_page+9]
-            unit2 = Monitor.objects.filter (monitor_name=res1)
-        if Monitor.objects.filter(monitor_type=res1).exists():
-            unit = Monitor.objects.filter(monitor_type=res1)[start_page-1:start_page+9]
-            unit2 = Monitor.objects.filter (monitor_type=res1)
-        if Monitor.objects.filter(editor=res1).exists():
-            unit = Monitor.objects.filter(editor=res1)[start_page-1:start_page+9]
-            unit2 = Monitor.objects.filter (editor=res1)
-        unit_len = len(unit2)
-        page_count = math.ceil(unit_len/10)
-        for i in unit:
-            dic = {
-                'id': i.id,
-                'monitor_name': i.monitor_name,
-                'monitor_type': i.monitor_type,
-                'editor': i.editor,
-                'edit_time': str(i.edit_time),
-                'font_size': i.font_size,
-                'height': i.height,
-                'width': i.width,
-                'status': i.status,
-                'start_time': str(i.start_time),
-                'end_time': str(i.end_time),
-                'period': i.period,
-                'page_count':page_count
-            }
-            res_list.append(dic)
-        return res_list
-    except Exception as e:
-        return None
+
+    res = json.loads(request.body)
+    res_list = []
+    res1 = "{}".format(res['data'])
+    limit = res['limit']
+    page = res['page']
+    unit =  Monitor.objects.filter(Q(monitor_type__icontains = res1)|Q(monitor_name__icontains = res1)| Q(editor__icontains = res1))
+    p = Paginator (unit, limit)  # 分页
+    page_count = p.page_range[-1]  # 总页数
+    page = p.page (page)  # 当前页数据
+    for i in page:
+        j = model_to_dict (i)
+        j['page_count'] = page_count
+        j['edit_time'] = str (i.edit_time)
+        j['create_time'] = str (i.create_time)
+        j['start_time'] = str (i.start_time)
+        j['end_time'] = str (i.end_time)
+        res_list.append (j)
+    return res_list
+    # except Exception as e:
+    #     return None
 
 
 def delete_unit(request):
@@ -125,6 +98,9 @@ def delete_unit(request):
 def add_unit(request):
     try:
         res = json.loads(request.body)
+        cilent = tools.interface_param (request)
+        user = cilent.bk_login.get_user({})
+
         monitor_type = res['monitor_type']
         print(monitor_type)
         if res['monitor_type'] == 'first':
@@ -140,8 +116,8 @@ def add_unit(request):
         add_dic['monitor_type'] = monitor_type
         add_dic['jion_id'] = None
         add_dic['status'] = 0
-        add_dic['creator'] = 'admin'
-        add_dic['editor'] = 'admin'
+        add_dic['creator'] = user['data']['bk_username']
+        add_dic['editor'] = user['data']['bk_username']
         Monitor.objects.create(**add_dic)
         result = tools.success_result(None)
     except Exception as e:
@@ -152,6 +128,8 @@ def add_unit(request):
 def edit_unit(request):
     try:
         res = json.loads (request.body)
+        cilent = tools.interface_param (request)
+        user = cilent.bk_login.get_user({})
         monitor_type = res['monitor_type']
         print(monitor_type)
         if res['monitor_type'] == 'first':
@@ -167,8 +145,7 @@ def edit_unit(request):
         add_dic['monitor_type'] = monitor_type
         add_dic['jion_id'] = None
         add_dic['status'] = 0
-        add_dic['creator'] = 'admin'
-        add_dic['editor'] = 'admin'
+        add_dic['editor'] = user['data']['bk_username']
         Monitor.objects.filter(monitor_name=res['monitor_name']).update(**add_dic)
         result = tools.success_result(None)
     except Exception as e:
