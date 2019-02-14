b5225dc128213880c9a5d4ccc0cdc4020c25371c
git <2221923603@qq.com>
2019-02-14 20:12:06 +0800

    告警校验规则调整

--- framework/alertRule/function.py
+++ framework/alertRule/function.py
@@ -9,14 +9,22 @@ from gatherData.models import *
 
 
 def rule_check():
+    #获取当前所有的采集数据
     gather_data = TDGatherData.objects.all()
     for data in gather_data:
         selected_rule = TbAlertRule.objects.filter(item_id=data.item_id, key_name=data.data_key).get()
-        print data.data_value
-        values = data.data_value.split(',')
-        for value in values:
-            if float(value) > selected_rule.upper_limit:
-                print 'FIELD : %s, ALERT!!!!! VALUE %s TOP OUT OF %s' % (data.data_key, value, selected_rule.upper_limit)
-            elif float(value) < selected_rule.lower_limit:
-                print 'FIELD : %s, ALERT!!!!! VALUE %s DOWN OUT OF %s' % (data.data_key, value, selected_rule.lower_limit)
+        # 只处理单值情况的告警，多值情况下的采集数据忽略
+        if ',' not in data.data_value:
+            # 告警规则配置了上限值和下限值的情况
+            if selected_rule.upper_limit is not None and selected_rule.lower_limit is not None:
+                if float(data.data_value) > selected_rule.upper_limit or float(data.data_value) < selected_rule.lower_limit:
+                    print 'FIELD : %s, ALERT!!!!! VALUE %s OUT OF RANGE' % (data.data_key, data.data_value)
+            # 告警规则仅配置了上限值的情况
+            elif selected_rule.upper_limit is not None and selected_rule.lower_limit is None:
+                if float(data.data_value) > selected_rule.upper_limit:
+                    print 'FIELD : %s, ALERT!!!!! VALUE %s OUT OF RANGE' % (data.data_key, data.data_value)
+            # 告警规则仅配置了下限值的情况
+            elif selected_rule.upper_limit is None and selected_rule.lower_limit is not None:
+                if float(data.data_value) < selected_rule.lower_limit:
+                    print 'FIELD : %s, ALERT!!!!! VALUE %s OUT OF RANGE' % (data.data_key, data.data_value)
     return "ok"
