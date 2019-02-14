c229b2bb095e7fa2ee8b76aee4c594c155f89d3d
git <2221923603@qq.com>
2019-02-14 18:59:59 +0800

    数据采集功能调整

--- framework/gatherData/function.py
+++ framework/gatherData/function.py
@@ -8,14 +8,15 @@ import MySQLdb
 import datetime
 from gatherData.models import *
 from gatherDataHistory.models import *
+from alertRule.function import *
 
 
 def gather_data(info):
     # 临时测试时用的数据，实际应从info对象中获取参数,由celery提供info对象
-    # info['id'] = '1'
-    # info['gather_params'] = 'sql'
-    # info['params'] = '46'
-    # info['gather_rule'] = 'SELECT china_point, japan_point FROM test_gather_data'
+    info['id'] = '1'
+    info['gather_params'] = 'sql'
+    info['params'] = '46'
+    info['gather_rule'] = 'SELECT china_point, japan_point FROM test_gather_data'
     # 获取数据采集的类型
     gather_type = info['gather_params']
     # 获取采集规则的字段有哪些
@@ -26,6 +27,7 @@ def gather_data(info):
         temp = dict()
         temp['key'] = field.strip()
         temp['value'] = list()
+        temp['value_str'] = ''
         data_set.append(temp)
     # 采集数据库中的数据
     if "sql" == gather_type:
@@ -50,12 +52,13 @@ def gather_data(info):
             count = 0
             for data in unit:
                 t = data_set[count]
-                t['value'].append(data)
+                t['value'].append(str(data))
+                t['value_str'] = ','.join(t['value'])
                 count += 1
-        print data_set
         # 将采集的数据保存到td_gather_data中
         for item in data_set:
-            TDGatherData(item_id=info['id'], gather_time=now, data_key=item['key'], data_value=item['value']).save()
+            TDGatherData(item_id=info['id'], gather_time=now, data_key=item['key'], data_value=item['value_str']).save()
+        # rule_check()
     elif "interface" == gather_type:
         pass# 接口方式采集数据待定
     else:
