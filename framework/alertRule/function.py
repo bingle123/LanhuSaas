00c3625d4d3e17acc23e5469e64f1b24a7afd748
git <2221923603@qq.com>
2019-02-14 18:56:41 +0800

    数据采集value值格式调整

--- framework/alertRule/function.py
+++ framework/alertRule/function.py
@@ -12,8 +12,11 @@ def rule_check():
     gather_data = TDGatherData.objects.all()
     for data in gather_data:
         selected_rule = TbAlertRule.objects.filter(item_id=data.item_id, key_name=data.data_key).get()
-        for value in data.data_value:
-            if value > selected_rule.upper_limit or value < selected_rule.lower_limit:
-                print 'FIELD : %s, ALERT!!!!!' % data.data_key
-                break
+        print data.data_value
+        values = data.data_value.split(',')
+        for value in values:
+            if float(value) > selected_rule.upper_limit:
+                print 'FIELD : %s, ALERT!!!!! VALUE %s TOP OUT OF %s' % (data.data_key, value, selected_rule.upper_limit)
+            elif float(value) < selected_rule.lower_limit:
+                print 'FIELD : %s, ALERT!!!!! VALUE %s DOWN OUT OF %s' % (data.data_key, value, selected_rule.lower_limit)
     return "ok"
