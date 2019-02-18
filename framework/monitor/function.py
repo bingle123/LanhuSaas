4e82f1ff63ecac60e5171d3ce442d4b6e993e55b
zhanglibing <zhanglibing@zork.com.cn>
2019-02-18 20:58:33 +0800

    预览

--- framework/monitor/function.py
+++ framework/monitor/function.py
@@ -19,6 +19,7 @@ import re
 from market_day import function
 from market_day import celery_opt as co
 from DataBaseManage.function import decrypt_str
+from gatherData import function as gatherdata
 
 
 def unit_show(request):
@@ -194,16 +195,26 @@ def edit_unit(request):
 def basic_test(request):
     res = json.loads(request.body)
     result = []
-    gather_rule = "select data_key,data_value from td_gather_data"
+    gather_rule2 = "select data_key,data_value from td_gather_data where item_id = 1"
     server_url = res['server_url']
+    gather_rule = res['gather_rule']
+    typeid = res['id']
+    gather_params = res['gather_params']
     sql = Conn.objects.get(id=server_url)
     password = f.decrypt_str(sql.password)
+    info = {
+        'id':typeid,
+        'gather_params':gather_params,
+        'params':server_url,
+        'gather_rule':gather_rule
+    }
+    gatherdata.gather_data(info)
     if sql.type == 'MySQL' or sql.type == 'Oracle':
         db = MySQLdb.connect(host=sql.ip, user=sql.username, passwd=password, db=sql.databasename, port=int(sql.port))
     if sql.type == 'SQL Server':
         db = pymssql.connect(sql.ip, sql.username, password, sql.databasename)
     cursor = db.cursor()
-    cursor.execute(gather_rule)
+    cursor.execute(gather_rule2)
     results = cursor.fetchall()
     dic = {}
     for i in results:
@@ -213,7 +224,8 @@ def basic_test(request):
         dic =  dict( dic, **dic1 )
     result.append(dic)
     db.close()
-    return results
+    return result
+
 
 
 def job_test(request):
