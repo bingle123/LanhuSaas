1d0e48669a4dac583c39a995fc913b3bd395bc69
git <2221923603@qq.com>
2019-02-20 16:42:25 +0800

    11

--- framework/gatherData/function.py
+++ framework/gatherData/function.py
@@ -39,15 +39,15 @@ def gather_test_init():
     # sql测试用类型：'sql'
     # 文件测试用类型：'file'
     # 接口测试用类型：'interface'
-    info['gather_params'] = 'interface'
+    info['gather_params'] = 'file'
     # sql测试用参数：'46'
     # 文件测试用参数：'192.168.1.10 /fk/test.txt'
-    # 接口测试用参数：'http://www.baidu.com,user=root&password=123'
-    info['params'] = 'http://t.weather.sojson.com/api/weather/city/,101030100'
+    # 接口测试用参数：'http://t.weather.sojson.com/api/weather/city/,101030100'
+    info['params'] = '192.168.1.10 /fk/test.txt'
     # sql测试用采集规则：'SELECT @cp=china_point@,@jp=japan_point@ FROM test_gather_data WHERE id=2'
-    # 文件测试用采集规则：'echo "1234"'
-    # 接口测试用采集规则：'接口采集规则'
-    info['gather_rule'] = 'url=$1\ncode=$2\nwget http://t.weather.sojson.com/api/weather/city/$code\ncat ./$code'
+    # 文件测试用采集规则：'echo {name: abc, age: 12}'
+    # 接口测试用采集规则：'url=$1\ncode=$2\nwget http://t.weather.sojson.com/api/weather/city/$code\ncat ./$code'
+    info['gather_rule'] = str('echo "{\"name\":\"zs\",\" age\":\"lisi\"}"')
     # ------------------------------------------------------------------------------------
     return info
 
@@ -134,8 +134,8 @@ def sql_kv_process(gather_field, sql_result):
     return data_set
 
 
-# 接口数据采集的key-value定义
-def interface_kv_process(json_dict):
+# 接口与接口数据采集的key-value定义
+def fi_kv_process(json_dict):
     data_set = list()
     for key, value in json_dict.items():
         temp = dict()
@@ -146,46 +146,6 @@ def interface_kv_process(json_dict):
         temp['key'] = key
         data_set.append(temp)
     return data_set
-
-
-# 遍历JSON字典中的所有key：json_dict需要递归的JSON字典，target_field目标采集字段，data_set采集结果，json_path当前遍历的JSON路径
-# def recursion_json_dict(json_dict, target_field, data_set, json_path):
-#     if isinstance(json_dict, dict):
-#         # 遍历，筛选，并将结果集整理为key-value形式的采集数据
-#         for key, value in json_dict.items():
-#             # 如果当前遍历的还是一个字典，递归遍历该字典
-#             if isinstance(value, dict):
-#                 # '%s.%s' % (json_path, key)使用.拼接当前key与JSON上层JSON路径，获得当前JSON路径
-#                 recursion_json_dict(value, target_field, data_set, '%s.%s' % (json_path, key))
-#             # 如果当期遍历的是一个数组，遍历当前数组，然后递归数组中的字典
-#             elif isinstance(value, list):
-#                 for v in value:
-#                     recursion_json_dict(v, target_field, data_set, '%s.%s' % (json_path, key))
-#             else:
-#                 json_path = '%s.%s' % (json_path, key)
-#                 # print "PATH: %s" % json_path
-#                 count = 0
-#                 for field in target_field:
-#                     # 获取需要采集的字段名称single_key
-#                     index = field.rfind('.')
-#                     if -1 != index:
-#                         single_key = field[index + 1:]
-#                     else:
-#                         single_key = field
-#                     # print 'FIELD: %s, SINGLE_KEY: %s, KEY: %s, JSON_PATH: %s' % (field, single_key, key, json_path)
-#                     # 判断当前遍历的key是否是需要采集的字段
-#                     if single_key == key:
-#                         # 判断当前的JSON路径是否符合需要采集的字段的JSON路径
-#                         if str(json_path).endswith(field):
-#                             # 保存当前JSON节点信息
-#                             t = data_set[count]
-#                             t['value'].append(str(value))
-#                             t['value_str'] = ','.join(t['value'])
-#                     count += 1
-#                 # print 'PATH: %s' % json_path
-#                 # 遍历完当前JSON节点后，回退到上一层节点继续遍历
-#                 index1 = json_path.rfind('.')
-#                 json_path = json_path[0:index1]
 
 
 # 采集方法，返回参数gather_status为ok采集正常，返回empty采集结果为空，返回error采集规则错误
@@ -279,7 +239,7 @@ def gather_data(info):
             gather_data_migrate(info['id'])
             # 将结果集整理为key-value形式的采集数据
             # recursion_json_dict(json_dict, gather_params['target_field'], data_set, gather_params['target_root'])
-            data_set = interface_kv_process(json_dict)
+            data_set = fi_kv_process(json_dict)
             # 将采集的数据保存到td_gather_data中
             for item in data_set:
                 TDGatherData(item_id=info['id'], gather_time=now, data_key=item['key'], data_value=item['value']).save()
@@ -295,6 +255,8 @@ def gather_data(info):
         # 获取采集参数
         gather_params = gather_param_parse(info)
         # 脚本内容，使用Base64编码
+        print 'DD: %s' % gather_params['gather_rule']
+        print 'AA: %s' %  base64.b64encode(gather_params['gather_rule'])
         script_content = base64.b64encode(gather_params['gather_rule'])
         script_params = json.dumps(gather_params['extra_param']['script_params'])
         print 'SCRIPT_PARAMS: %s' % script_params
@@ -305,11 +267,9 @@ def gather_data(info):
         # 蓝鲸Agent所在IP地址，暂固定为192.168.1.52
         agent_id = '192.168.1.52'
         # 向蓝鲸平台请求执行快速执行脚本
-        bk_params = {
+        script_bk_params = {
             'bk_biz_id': biz_id,
             'script_content': script_content,
-            'script_param': script_params,
-            'is_param_sensitive': 0,
             'account': 'root',
             'script_type': 1,
             'ip_list': [
@@ -319,7 +279,34 @@ def gather_data(info):
                 }
             ]
         }
-        res = client.job.fast_execute_script(bk_params)
+        res = client.job.fast_execute_script(script_bk_params)
+        # 向蓝鲸平台请求执行作业平台日志
+        job_log_bk_params = {
+            'bk_biz_id': biz_id,
+            'job_instance_id': res['data']['job_instance_id']
+        }
+        r = client.job.get_job_instance_log(job_log_bk_params)
+        while 'True' != str(r['data'][0]['is_finished']):
+            time.sleep(3)
+            r = client.job.get_job_instance_log(job_log_bk_params)
+        # 获取当前采集时间
+        now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        json_data = r['data'][0]['step_results'][0]['ip_logs'][0]['log_content']
+        print json_data.encode('utf-8')
+        json_dict = json.loads(json_data.encode('utf-8'))
+        # 判断文件是否返回了空数据
+        if 0 != len(json_dict):
+            # 历史采集数据迁移
+            gather_data_migrate(info['id'])
+            # 将结果集整理为key-value形式的采集数据
+            # recursion_json_dict(json_dict, gather_params['target_field'], data_set, gather_params['target_root'])
+            data_set = fi_kv_process(json_dict)
+            # 将采集的数据保存到td_gather_data中
+            for item in data_set:
+                TDGatherData(item_id=info['id'], gather_time=now, data_key=item['key'], data_value=item['value']).save()
+        else:
+            TDGatherData(item_id=info['id'], gather_time=now, data_key='URL_CONNECTION', data_value='0').save()
+            return 'empty'
         if 'success' != res['message']:
             return 'error'
     # 数据采集完毕后使用告警规则检查数据合法性
