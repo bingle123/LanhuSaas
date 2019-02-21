b8db7d5faf2c9ccd87bedb9f92ee52caa18a7385
zlx <975495461@qq.com>
2019-02-21 17:14:26 +0800

    zz

--- framework/market_day/views.py
+++ framework/market_day/views.py
@@ -7,8 +7,8 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.cache import cache_page
 import tasks
+from monitor import tools
 import os
-from shell_app import tools
 def get_holiday(req):
     days=function.get_holiday(req)
     return render_json(days)
@@ -33,13 +33,38 @@ def addone(req,date):
     return render_json(flag)
 
 def cedemo(req):
-    client=tools.interface_param(req)
     param={
-        "bk_biz_id": "2",
-        "task_id": "15"
+       'monitor_name':'frg',
+       'monitor_type':'流程单元类型',
+       'template_id':5,
+        'node_times':[
+            {'starttime':'15:30',
+             'endtime':'16:30'
+             },
+            {'starttime':'15:30',
+             'endtime':'16:30'
+             }
+        ],
+        'template_name':'CY流程测试',
+        'period':10,
     }
-    res=client.sops.get_task_status(param)
-    return render_json(res['data']['children'])
+    info = {
+        'id': 51,
+        'template_id': 5,  # 创建任务的模板id
+        'node_times':[
+            {'starttime':'15:30',
+             'endtime':'16:30'
+             },
+            {'starttime':'15:30',
+             'endtime':'16:30'
+             }
+        ],
+        'template_name': 'CY流程测试',
+        'period':100
+    }
+    p={"item_id": 51, "node_times": [{"endtime": "16:30", "starttime": "15:30"}, {"endtime": "16:30", "starttime": "15:30"}], "task_id": 31}
+    tools.flow_gather_task(**p)
+    return render_json('ok')
 def statusdemo(req):
     function.add_unit_task()
     return HttpResponse('ok')
