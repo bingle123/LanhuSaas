53d2bad0e2f9b77eef710f9f459c55cec166928d
git <2221923603@qq.com>
2019-02-22 16:07:45 +0800

    采集规则表达式调整

--- framework/gatherData/function.py
+++ framework/gatherData/function.py
@@ -43,13 +43,13 @@ def gather_test_init():
     # 接口测试用类型：'interface'
     info['gather_params'] = 'interface'
     # sql测试用参数：'46'
-    # 文件测试用参数：'192.168.1.52,./gather_data_test'
-    # 接口测试用参数：'http://t.weather.sojson.com/api/weather/city/101030100,http://t.weather.sojson.com/api/weather/city/ 101030100'
-    info['params'] = 'http://t.weather.sojson.com/api/weather/city/101030100,http://t.weather.sojson.com/api/weather/city/ 101030100'
+    # 文件测试用参数：'192.168.1.52#{"file_path" : "./gather_data_test"}'
+    # 接口测试用参数：'http://t.weather.sojson.com/api/weather/city/101030100#{"url": "http://t.weather.sojson.com/api/weather/city/", "code": "101030100"}'
+    info['params'] = 'http://t.weather.sojson.com/api/weather/city/101030100#{"url": "http://t.weather.sojson.com/api/weather/city/", "code": "101030100"}'
     # sql测试用采集规则：'SELECT @cp=china_point@,@jp=japan_point@ FROM test_gather_data WHERE id=2'
-    # 文件测试用采集规则：'cGF0aD0kMQpjYXQgJHBhdGgK'
+    # 文件测试用采集规则：'cat ${file_path}'
     # 接口测试用采集规则：'dXJsPSQxCmNvZGU9JDIKYHdnZXQgLXFPIGdhdGhlcl9kYXRhX3RlbXAgJHVybCRjb2RlYApjYXQgZ2F0aGVyX2RhdGFfdGVtcAo='
-    info['gather_rule'] = 'dXJsPSQxCmNvZGU9JDIKYHdnZXQgLXFPIGdhdGhlcl9kYXRhX3RlbXAgJHVybCRjb2RlYApjYXQgZ2F0aGVyX2RhdGFfdGVtcAo='
+    info['gather_rule'] = 'cat ${file_path}'
     # ------------------------------------------------------------------------------------
     return info
 
@@ -84,18 +84,22 @@ def gather_param_parse(info):
         conn_info.password = decrypt_str(conn_info.password)
         gather_params['extra_param']['connection_param'] = conn_info
     elif 'interface' == info['gather_params']:
-        interface_params = info['params'].split(',')
-        # 获取接口url参数
-        interface_url = interface_params[0]
+        interface_params = info['params'].split('#')
+        print interface_params
         # 获取调用接口所需的参数
-        interface_params = interface_params[1]
-        gather_params['extra_param']['interface_url'] = interface_url
-        gather_params['extra_param']['script_params'] = interface_params
+        json_params = json.loads(interface_params[1])
+        for key, value in json_params.items():
+            info['gather_rule'] = info['gather_rule'].replace('${%s}' % key, value)
+            gather_params['extra_param']['url'] = interface_params[0]
         gather_params['gather_rule'] = info['gather_rule']
     elif 'file' == info['gather_params']:
-        file_params = info['params'].split(',')
+        file_params = info['params'].split('#')
+        json_params = json.loads(file_params[1])
+        for key, value in json_params.items():
+            if 'file_path' == key:
+                gather_params['extra_param']['file_path'] = value
+                info['gather_rule'] = info['gather_rule'].replace('${%s}' % key, value)
         gather_params['extra_param']['file_server'] = file_params[0]
-        gather_params['extra_param']['script_params'] = file_params[1]
         gather_params['gather_rule'] = info['gather_rule']
     elif 'space_interface' == info['gather_params']:
         pass
@@ -203,18 +207,20 @@ def gather_data(info):
             TDGatherData(item_id=info['id'], gather_time=now, data_key='DB_CONNECTION', data_value='0').save()
             return "empty"
     elif "interface" == gather_type:
+        # 历史采集数据迁移
+        gather_data_migrate(info['id'])
         # 获取当前采集时间
         now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        ping_script = 'aXA9JDEKYHdnZXQgLXQgMSAtVCA1MDAgLS1zcGlkZXIgLW8gdGVtcCAkaXBgCmNvdW50PWBjYXQgdGVtcCB8IGdyZXAgIjIwMCBPSyIgfCB3YyAtbGAKaWYgWyAkY291bnQgLWd0IDAgXTsgdGhlbgogICAgZWNobyAiMCIKZWxzZQogICAgZWNobyAiLTEiCmZpCg=='
         # 接口方式采集数据
         user_account = BkUser.objects.filter(id=1).get()
         # 根据id为1的用户获取客户端操作快速执行脚本
         client = get_client_by_user(user_account)
         client.set_bk_api_ver('v2')
-        # 历史采集数据迁移
-        gather_data_migrate(info['id'])
+        # 测试开启
+        # info['gather_rule'] = load_script_content('URL_TEST')
         gather_params = gather_param_parse(info)
-        res1 = execute_script(client, ping_script, base64.b64encode(gather_params['extra_param']['interface_url']), info['id'], 'URL_CONNECTION', settings.GATHER_DATA_HOST)
+        ping_script = base64.b64encode(load_script_content('URL_CONNECTION').replace('${url}', gather_params['extra_param']['url']))
+        res1 = execute_script(client, ping_script, None, info['id'], 'URL_CONNECTION', settings.GATHER_DATA_HOST)
         if None is res1:
             return "error"
         res2 = get_execute_result(client, res1['data']['job_instance_id'], info['id'], 'URL_CONNECTION')
@@ -228,9 +234,7 @@ def gather_data(info):
         # 接口连接状态为正常
         TDGatherData(item_id=info['id'], gather_time=now, data_key='URL_CONNECTION', data_value='1').save()
         # 发送请求，从接口获取JSON数据
-        res3 = execute_script(client, base64.b64encode(gather_params['gather_rule']), base64.b64encode(gather_params['extra_param']['script_params']), info['id'], 'URL_CONNECTION', settings.GATHER_DATA_HOST)
-        # 测试打开
-        # res3 = execute_script(client, gather_params['gather_rule'], base64.b64encode(gather_params['extra_param']['script_params']), info['id'], 'URL_CONNECTION', settings.GATHER_DATA_HOST)
+        res3 = execute_script(client, base64.b64encode(gather_params['gather_rule']), None, info['id'], 'URL_CONNECTION', settings.GATHER_DATA_HOST)
         if None is res3:
             return "error"
         res4 = get_execute_result(client, res3['data']['job_instance_id'], info['id'], 'URL_CONNECTION')
@@ -253,14 +257,6 @@ def gather_data(info):
             TDGatherData(item_id=info['id'], gather_time=now, data_key='URL_CONNECTION', data_value='0').save()
             return 'empty'
     elif "file" == gather_type:
-        # content = ''
-        # with open('./file_gather_data.sh') as f:
-        #     line = f.readline()
-        #     while line:
-        #         content = content + line
-        #         line = f.readline()
-        # print content
-        # print base64.b64encode(content)
         # 历史采集数据迁移
         gather_data_migrate(info['id'])
         # 获取当前采集时间
@@ -270,12 +266,12 @@ def gather_data(info):
         # 根据id为1的用户获取客户端操作快速执行脚本
         client = get_client_by_user(user_account)
         client.set_bk_api_ver('v2')
+        # 测试开启
+        # info['gather_rule'] = load_script_content('FILE_TEST')
         # 获取采集参数
         gather_params = gather_param_parse(info)
-        script_params = base64.b64encode(gather_params['extra_param']['script_params'])
-        # print 'SCRIPT_PARAMS: %s' % script_params
-        file_exist_script = 'cGF0aD0kMQppZiBbICEgLWYgIiRwYXRoIiBdOyB0aGVuCiAgICBlY2hvICItMSIKZWxzZQogICAgZWNobyAiMCIKZmkK'
-        res1 = execute_script(client, file_exist_script, script_params, info['id'], 'FILE_EXIST', gather_params['extra_param']['file_server'])
+        file_exist_script = base64.b64encode(load_script_content('FILE_EXIST').replace('${file_path}', gather_params['extra_param']['file_path']))
+        res1 = execute_script(client, file_exist_script, None, info['id'], 'FILE_EXIST', gather_params['extra_param']['file_server'])
         if None is res1:
             return "error"
         res2 = get_execute_result(client, res1['data']['job_instance_id'], info['id'], 'FILE_EXIST')
@@ -283,15 +279,12 @@ def gather_data(info):
             return "error"
         # 检测文件是否存在
         file_flag = str(res2['data'][0]['step_results'][0]['ip_logs'][0]['log_content'])
-        # print 'FILE %s' % file_flag
         if -1 == int(file_flag):
             TDGatherData(item_id=info['id'], gather_time=now, data_key='FILE_EXIST', data_value='-1', gather_error_log='file not exist').save()
             return "error"
         # 文件状态存在
         TDGatherData(item_id=info['id'], gather_time=now, data_key='FILE_EXIST', data_value='1').save()
-        res3 = execute_script(client, base64.b64encode(gather_params['gather_rule']), script_params, info['id'], 'FILE_EXIST', gather_params['extra_param']['file_server'])
-        # 测试打开
-        # res3 = execute_script(client, gather_params['gather_rule'], script_params, info['id'], 'FILE_EXIST', gather_params['extra_param']['file_server'])
+        res3 = execute_script(client, base64.b64encode(gather_params['gather_rule']), None, info['id'], 'FILE_EXIST', gather_params['extra_param']['file_server'])
         if None is res3:
             return "error"
         res4 = get_execute_result(client, res3['data']['job_instance_id'], info['id'], 'FILE_EXIST')
@@ -377,3 +370,20 @@ def get_execute_result(client, job_instance_id, item_id, execute_type):
         return None
     return res
 
+
+def load_script_content(script_type):
+    if 'FILE_EXIST' == script_type:
+        script_path = './static/script/file_exist.sh'
+    elif 'URL_CONNECTION' == script_type:
+        script_path = './static/script/interface_ping.sh'
+    elif 'FILE_TEST' == script_type:
+        script_path = './static/script/file_gather_data.sh'
+    elif 'URL_TEST' == script_type:
+        script_path = './static/script/interface_gather_data.sh'
+    script_content = ''
+    with open(script_path) as f:
+        line = f.readline()
+        while line:
+            script_content = script_content + line
+            line = f.readline()
+    return script_content
