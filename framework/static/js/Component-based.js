15b0e0dbeb3fd9407a01c3f402857bf95205ad3d
JacksonLiu <liuxinpeng.222@foxmail.com>
2019-02-28 19:16:05 +0800

    场景编排前台

--- framework/static/js/Component-based.js
+++ framework/static/js/Component-based.js
@@ -269,8 +269,12 @@ function base_monitor(item_id,font_size,height,width,content) {
     // $('[type='+selector_id+']').css('height',height);
     // $('[type='+selector_id+']').css('width',width);
     // },dataType='json')
-        $('[type='+selector_id+']').html("")
-        $('[type='+selector_id+']').append("<div>"+content +"</div>")
+        $('[type='+selector_id+']').html("");
+        $('[type='+selector_id+']').append("<div>"+content +"</div>");
+        $('[type='+selector_id+']').append('<input class="score_input" type="text">');
+        $('[type='+selector_id+']').append('<div class="right_click"><span class="score">打分</span><span class="delete">删除监控项</span></div>');
+        $('[type='+selector_id+']').css('height',height+'px');
+        $('[type='+selector_id+']').css('width',width+'px');
         // if(content.indexOf("#") > -1 && content.indexOf('@') == -1){
         //     var temp1 = content.split("#");
         //     var temp2=[];
@@ -484,6 +488,8 @@ function flow_monitor(value1,value2){
                             "                                    </div>\n" +
                             "                            </div>";
                         $('[type='+selector_type+']').html(cc);
+                        $('[type='+selector_type+']').append('<input class="score_input" type="text">');
+                        $('[type='+selector_type+']').append('<div class="right_click"><span class="score">打分</span><span class="delete">删除监控项</span></div>');
                 axios({
                     method:'post',
                     url:'/monitor_item/flow_change/',
