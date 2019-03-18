10c4dd438714544c1321665d3aee1e033e365b7a
zlx <975495461@qq.com>
2019-03-18 09:08:41 +0800

    ccc

--- framework/market_day/function.py
+++ framework/market_day/function.py
@@ -1,6 +1,6 @@
 #!usr/bin/ebv python
 # -*- coding:utf-8 -*-
-from models import Holiday,HeaderData,Area
+from models import Holiday, HeaderData, Area
 import os
 from xlrd import open_workbook
 from conf import default
@@ -14,15 +14,16 @@ import pytz
 from shell_app.function import get_user
 
 
-def get_holiday(req,area):
-    dates = Holiday.objects.filter(Q(flag=0)&Q(area=int(area))).values('day')
+def get_holiday(req, area):
+    dates = Holiday.objects.filter(Q(flag=0) & Q(area=int(area))).values('day')
     days = []
     for date in dates:
         days.append(date['day'])
     return days
 
-#通过节假日文件获取节假日
-def get_file(req,area):
+
+# 通过节假日文件获取节假日
+def get_file(req, area):
     if req.method == 'POST':
         try:
             obj = req.FILES.get('file')
@@ -40,59 +41,66 @@ def get_file(req,area):
                 day = str(sheet.row_values(i)[0])
                 d = day[0:4] + u'/' + str(int(day[4:6])) + u'/' + str(int(day[6:8]))
                 flag = int(sheet.row_values(i)[1])
-                holiday = Holiday(day=d, flag=flag,area=area)
+                holiday = Holiday(day=d, flag=flag, area=area)
                 holiday.save()
         except:
             print '文件不匹配'
 
-#删除所有的节假日
+
+# 删除所有的节假日
 def delall(area):
     flag = Holiday.objects.filter(area=int(area)).delete()
     return flag
 
-#删除指定的节假日
+
+# 删除指定的节假日
 def delone(req):
-    res=json.loads(req.body)
-    date=res['date']
-    area=res['area']
-    flag = Holiday.objects.filter(Q(day=date)& Q(area=int(area))).update(flag=1)
+    res = json.loads(req.body)
+    date = res['date']
+    area = res['area']
+    flag = Holiday.objects.filter(Q(day=date) & Q(area=int(area))).update(flag=1)
     return flag
 
-#添加一个日期为节假日
+
+# 添加一个日期为节假日
 def addone(req):
     res = json.loads(req.body)
     date = res['date']
     area = res['area']
-    day=Holiday.objects.get(Q(day=date)&Q(area=int(area)))
-    day.flag=0
+    day = Holiday.objects.get(Q(day=date) & Q(area=int(area)))
+    day.flag = 0
     day.save()
     return 'ok'
 
-#定时任务demo
+
+# 定时任务demo
 def addperdic_task():
-    flag=co.create_task_interval(name='demo_per', task='market_day.tasks.count_time', task_args=[10,50], desc='demodemo',interval_time={'every':10,'period':'seconds'})
+    flag = co.create_task_interval(name='demo_per', task='market_day.tasks.count_time', task_args=[10, 50],
+                                   desc='demodemo', interval_time={'every': 10, 'period': 'seconds'})
     return flag
 
-#添加一个监控项定时任务
+
+# 添加一个监控项定时任务
 def add_unit_task(add_dicx):
     # print add_dicx
-    type=add_dicx['monitor_type']
+    type = add_dicx['monitor_type']
+    print type
     schename = add_dicx['monitor_name']
-    id=Monitor.objects.filter(monitor_name=schename).last().id
-    schename=str(id)
+    id = Monitor.objects.filter(monitor_name=schename).last().id
+    schename = str(id)
     starthour = str(add_dicx['start_time']).split(':')[0]
     startmin = str(add_dicx['start_time']).split(':')[1]
     endtime = add_dicx['end_time']
-    #创建一个特定时区的时间的实例
-    temp_date=datetime(2019,2,12,int(starthour),int(startmin),0)
+    # 创建一个特定时区的时间的实例
+    temp_date = datetime(2019, 2, 12, int(starthour), int(startmin), 0)
     timezone = Area.objects.get(id=add_dicx['monitor_area']).timezone
-    #将开始时间转化为对应时区的时间
-    starthour,startmin=tran_time_china(temp_date,timezone=timezone)
-    temp_date=datetime(2019,2,12,int(endtime.split(':')[0]),int(endtime.split(':')[1]),0)
-    endhour,endmin=tran_time_china(temp_date,timezone=timezone)
-    endtime=endhour+":"+endmin
+    # 将开始时间转化为对应时区的时间
+    starthour, startmin = tran_time_china(temp_date, timezone=timezone)
+    temp_date = datetime(2019, 2, 12, int(endtime.split(':')[0]), int(endtime.split(':')[1]), 0)
+    endhour, endmin = tran_time_china(temp_date, timezone=timezone)
+    endtime = endhour + ":" + endmin
     period = int(add_dicx['period'])
-    if type==1:
+    if type == 1:
         ctime = {
             'hour': starthour,
             'minute': startmin,
@@ -102,14 +110,15 @@ def add_unit_task(add_dicx):
             'gather_params': add_dicx['gather_params'],
             'params': add_dicx['params'],
             'gather_rule': add_dicx['gather_rule'],
-            'period':period,
-            'area_id':add_dicx['monitor_area'],
-            'task_name':str(schename)+'task',
-            'endtime':endtime
+            'period': period,
+            'area_id': add_dicx['monitor_area'],
+            'task_name': str(schename) + 'task',
+            'endtime': endtime
         }
-        #创建一个基本监控项采集的开始任务
-        co.create_task_crontab(name=schename, task='market_day.tasks.gather_data_task_one', crontab_time=ctime,task_args=info, desc=schename)
-    elif type==2:
+        # 创建一个基本监控项采集的开始任务
+        co.create_task_crontab(name=schename, task='market_day.tasks.gather_data_task_one', crontab_time=ctime,
+                               task_args=info, desc=schename)
+    elif type == 2:
         ctime = {
             'hour': starthour,
             'minute': startmin,
@@ -124,55 +133,58 @@ def add_unit_task(add_dicx):
             'task_name': str(schename) + 'task',
             'endtime': endtime
         }
-        #创建一个图标采集开始任务
+        # 创建一个图标采集开始任务
         co.create_task_crontab(name=schename, task='market_day.tasks.gather_data_task_one', crontab_time=ctime,
                                task_args=info, desc=schename)
 
-    elif type==3:
+    elif type == 3:
         ctime = {
             'hour': starthour,
             'minute': startmin,
         }
         info = {
             'id': id,
-            'params' : add_dicx['params'],  # ip
-            'gather_params' : add_dicx['gather_params'],
+            'params': add_dicx['params'],  # ip
+            'gather_params': add_dicx['gather_params'],
             'job_id': [{
                 'name': add_dicx['gather_rule'],
                 'id': add_dicx['jion_id']
             }],
-            'period':period,
+            'period': period,
             'area_id': add_dicx['monitor_area'],
             'task_name': str(schename) + 'task',
             'endtime': endtime
         }
-        #创建一个作业采集的开始任务
-        co.create_task_crontab(name=schename, task='market_day.tasks.gather_data_task_two', crontab_time=ctime,task_args=info, desc=schename)
-    elif type==4:
-        template_list=add_dicx['jion_id']
-        node_times=add_dicx['node_times']
-        constants=add_dicx['constants']
+        # 创建一个作业采集的开始任务
+        co.create_task_crontab(name=schename, task='market_day.tasks.gather_data_task_two', crontab_time=ctime,
+                               task_args=info, desc=schename)
+    elif type == 'fourth':
+        template_list = add_dicx['jion_id']
+        node_times = add_dicx['node_times']
+        constants = add_dicx['constants']
         ctime = {
             'hour': starthour,
             'minute': startmin,
         }
         info = {
             'id': id,
-            'template_id':template_list,#创建任务的模板id
-            'node_times':node_times,
-            'period':period,
-            'constants':constants,
+            'template_id': template_list,  # 创建任务的模板id
+            'node_times': node_times,
+            'period': period,
+            'constants': constants,
             'area_id': add_dicx['monitor_area'],
             'task_name': str(schename) + 'task',
             'endtime': endtime
         }
-        #创建一个开始流程的任务
-        co.create_task_crontab(name=schename, task='market_day.tasks.start_flow_task', crontab_time=ctime,task_args=info, desc=schename)
+        # 创建一个开始流程的任务
+        co.create_task_crontab(name=schename, task='market_day.tasks.start_flow_task', crontab_time=ctime,
+                               task_args=info, desc=schename)
 
-#获取蓝鲸平台的头文件，并存入数据库
+
+# 获取蓝鲸平台的头文件，并存入数据库
 def get_header_data(request):
-    role_id=get_user(request)['data']['bk_role']
-    h,flag=HeaderData.objects.get_or_create(id=1)
+    role_id = get_user(request)['data']['bk_role']
+    h, flag = HeaderData.objects.get_or_create(id=1)
     headers = {
         "Content-Type": 'application/json;charset=utf-8',
         "Cookie": 'csrftoken=bNAyZ7pBsJ1OEi8TMq1NqxNXY2CUREEO; sessionid=r9g2ofn1wb0ykd1epg8crk9l5pgyeuu2; bk_csrftoken=GdxslZh1U3YVsCthqXIv09PbVoW0AaQd; bklogin_csrftoken=z8goJXIMXil80lFT3VtLQHMClrPIExl9; blueking_language=zh-cn; bk_token=kxgoYlRp77AkbGVX85AdFVR0t6eqqHeJ-BlMXxA6oM0',
@@ -187,71 +199,78 @@ def get_header_data(request):
         Cookie = "%s;%s=%s" % (Cookie, key, request.COOKIES[key]);
     headers["Cookie"] = Cookie;
     headers["X-CSRFToken"] = csrftoken;
-    #如果当前用户不是管理员无法访问v3接口不保存header信息
-    if role_id==1:
-        h.header=json.dumps(headers, ensure_ascii=False)
+    # 如果当前用户不是管理员无法访问v3接口不保存header信息
+    if role_id == 1:
+        h.header = json.dumps(headers, ensure_ascii=False)
         h.save()
 
-#添加一个新的日历地区
+# 添加一个新的日历地区
 def add_area(req):
-    res=json.loads(req.body)
-    name=res['country']
-    timezone=res['timezone']
-    a,flag=Area.objects.get_or_create(country=name)
-    a.timezone=timezone
+    res = json.loads(req.body)
+    name = res['country']
+    timezone = res['timezone']
+    a, flag = Area.objects.get_or_create(country=name)
+    a.timezone = timezone
     a.save()
     return 'ok'
 
-#获取所有的日历地区
+
+# 获取所有的日历地区
 def get_all_area(req):
-    areas=Area.objects.all()
-    area_dict=[]
+    areas = Area.objects.all()
+    area_dict = []
     for a in areas:
         area_dict.append(model_to_dict(a))
     return area_dict
 
-#删除日历的某个地区
+
+# 删除日历的某个地区
 def del_area(name):
     Area.objects.get(id=name).delete()
     Holiday.objects.filter(area=name).delete()
     return 'ok'
 
-#获得世界上的所有时区
+
+# 获得世界上的所有时区
 def get_all_timezone():
-    all=pytz.common_timezones
+    all = pytz.common_timezones
     return all
 
-#判断今天是不是对应地区的工作日
+
+# 判断今天是不是对应地区的工作日
 def check_jobday(id):
-    timezone=Area.objects.get(id=id).timezone
-    tz=pytz.timezone(timezone)
-    now=datetime.now(tz)
-    str_date=datetime.strftime(now,'%Y/%m/%d')
-    day=str_date[:4] + u'/' + str(int(str_date[5:7])) + u'/' + str(int(str_date[8:10]))
-    hs=Holiday.objects.filter(Q(day=day)&Q(area=id))
-    flag=0
+    timezone = Area.objects.get(id=id).timezone
+    tz = pytz.timezone(timezone)
+    now = datetime.now(tz)
+    str_date = datetime.strftime(now, '%Y/%m/%d')
+    day = str_date[:4] + u'/' + str(int(str_date[5:7])) + u'/' + str(int(str_date[8:10]))
+    hs = Holiday.objects.filter(Q(day=day) & Q(area=id))
+    flag = 0
     for h in hs:
-        flag=h.flag
-    if flag==1:
+        flag = h.flag
+    if flag == 1:
         return True
     else:
         return False
-#将不同时区的时间转为中国时间
-def tran_time_china(tempdate,timezone):
+
+
+# 将不同时区的时间转为中国时间
+def tran_time_china(tempdate, timezone):
     central = pytz.timezone('Asia/Shanghai')
     local_us = central.localize(tempdate)
     # 使用astimezone得出时间
     time = local_us.astimezone(pytz.timezone(timezone))
-    return str(time.hour),str(time.minute)
+    return str(time.hour), str(time.minute)
 
-#将中国时间转为不同的时区
-def tran_china_time_other(time,timezone):
-    hour=time.hour
-    min=time.minute
-    tempdate=datetime(2019,1,2,int(hour),int(min),0)
-    timezone=Area.objects.get(id=timezone).timezone
+
+# 将中国时间转为不同的时区
+def tran_china_time_other(time, timezone):
+    hour = time.hour
+    min = time.minute
+    tempdate = datetime(2019, 1, 2, int(hour), int(min), 0)
+    timezone = Area.objects.get(id=timezone).timezone
     central = pytz.timezone(timezone)
     local_us = central.localize(tempdate)
     # 使用astimezone得出时间
     time = local_us.astimezone(pytz.timezone('Asia/Shanghai'))
-    return str(time.hour)+":"+str(time.minute)
+    return str(time.hour) + ":" + str(time.minute)
--- framework/market_day/tasks.py
+++ framework/market_day/tasks.py
@@ -2,10 +2,8 @@
 # -*- coding:utf-8 -*-
 from __future__ import absolute_import
 from celery import task
-from system_config.crawl_template import crawl_temp
-from system_config.function import *
+from system_config.function import test
 from celery.task import periodic_task
-from system_config.models import SendMailLog as sml
 from djcelery import models as celery_models
 import logging
 from gather_data import function
@@ -25,67 +23,8 @@ from history_chart.function import select_scene_operation
 
 @task
 def crawl_task(**i):
-    id = i['id']
-    crawl_url = i['crawl_url']
-    crawl_name = i['crawl_name']
-    total_xpath = i['total_xpath']
-    title_xpath = i['title_xpath']
-    url_xpath = i['url_xpath']
-    time_xpath = i['time_xpath']
-    crawl_keyword = i['crawl_keyword']
-    crawl_no_keyword = i['crawl_no_keyword']
-    url_pre = i['url_pre']
-    # 接收人--列表
-    receivers = i['receivers'].split('@')
-    # 开始爬虫
-    crawl_result = crawl_temp(crawl_url, total_xpath, title_xpath, time_xpath, url_xpath)
-    # 爬虫成功，且有数据
-    print crawl_result
-    if crawl_result['code'] == 0 and crawl_result['results'].__len__() != 0:
-        send_result = []
-        for j in range(crawl_result['results'].__len__()):
-            # 增加爬虫配置ID
-            crawl_result['results'][j].update(crawl_id=id)
-            # 增加爬虫推送人---用户名需要转换成邮箱地址
-            crawl_result['results'][j].update(receivers=receivers)
-            # 拼接URL
-            if crawl_result['results'][j]['resource'][0:4] == 'http':
-                # 若resource自带http则不操作
-                pass
-            else:
-                # 若resource不自带http则增加前缀
-                crawl_result['results'][j]['resource'] = url_pre + crawl_result['results'][j]['resource']
-            # 爬取内容包含关键字并且不包含非关键字的数据，并加入到结果集
-            if crawl_keyword in crawl_result['results'][j]['title'] and crawl_no_keyword not in \
-                    crawl_result['results'][j]['title']:
-                res = CrawlContent.objects.filter(title_content=crawl_result['results'][j]['title'])
-                # 爬取内容筛选数据库中不存在的内容增加到result_all
-                if len(res) == 0:
-                    # 增加到结果集
-                    crawl_id = crawl_result['results'][j]['crawl_id']
-                    title = crawl_result['results'][j]['title']
-                    resource = crawl_result['results'][j]['resource']
-                    time = crawl_result['results'][j]['time']
-                    # 保存爬虫内容
-                    CrawlContent.objects.create(crawl_id=crawl_id, title_content=title, url_content=resource,
-                                                time_content=time)
-                    # 此处为接收人的邮箱日后需要从清算园里查询出来,这里为测试数据
-                    receivers_mail = ['761494073@qq.com', 'liaomingtao@zork.com.cn']
-                    send_result.append(crawl_result['results'][j])
-                    # send_content = change_to_html(crawl_result['results'][j])
-                    # theme = crawl_name + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + u'的爬虫信息'
-                    # mail_send(theme, send_content, receivers_mail)
-        if len(send_result) == 0:
-            # 内容为空，不需要发送
-            pass
-        else:
-            # print send_result
-            receivers_mail = ['761494073@qq.com', 'liaomingtao@zork.com.cn']
-            theme = crawl_name + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + u'的爬虫信息'
-            send_content = change_to_html(send_result)
-            mail_send(theme, send_content, receivers_mail)
-            sml.objects.create(link_id=id, message_title=theme, message_content=send_content)
-            logging.error(u'消息日志保存成功')
+    print u'爬虫开始'
+    test()
     return 'success'
 
 #基本监控项和图表监控项的采集开始task
@@ -171,6 +110,7 @@ def gather_data_task_thrid(**i):
         # 调用流程监控项数据采集的方法
         tools.flow_gather_task(**i)
     else:
+        print u'删除'+task_name
         co.delete_task(task_name)
 
 #流程监控项的采集测试专用task
@@ -222,7 +162,7 @@ def start_flow_task(**info):
                 'item_id': info['id'],
                 'task_id': task_id,  # 启动流程的任务id
                 'node_times': node_times,
-                'task_name':info['task_name'] + '_check_status_test',
+                'task_name':info['task_name'] + 'task',
                 'endtime': info['endtime']
             }
             period = {
