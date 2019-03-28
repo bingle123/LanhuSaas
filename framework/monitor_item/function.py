7527b0d909aa6e645d1b9f32fe7f30a7c248e739
X230 <343434632@qq.com>
2019-03-28 14:24:56 +0800

    liuxichun

--- framework/monitor_item/function.py
+++ framework/monitor_item/function.py
@@ -110,8 +110,8 @@ def select_unit(request):
     res1 = res['data']
     limit = res['limit']
     page = res['page']
-    # 模糊查询
-    unit = Monitor.objects.filter(Q(monitor_name__icontains=res1) | Q(editor__icontains=res1))
+    # 模糊查询,根据id倒排序
+    unit = Monitor.objects.filter(Q(monitor_name__icontains=res1) | Q(editor__icontains=res1)).order_by("-id")
     page_data, base_page_count = tools.page_paging(unit, limit, page)
     res_list = tools.obt_dic(page_data, base_page_count)
     res_data = tools.success_result(res_list)
