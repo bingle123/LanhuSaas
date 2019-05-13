58f659c97b0133af6c4581d2ea6c41e1dd49c57d
liuqing.jia@detour.tech <liuqing.jia@detour.tech>
2019-05-13 11:19:07 +0800

    jlq-2019-05-13-1118

--- framework/home_page/function.py
+++ framework/home_page/function.py
@@ -15,6 +15,7 @@ from datetime import datetime
 from models import *
 from system_config.models import  *
 from db_connection.function import *
+from gather_data.models import TDGatherData
 
 
 def get_time(request):
@@ -54,6 +55,7 @@ def scenes_alert(request):
         null_data = 'n'
         return tools.success_result(null_data)
     alert_log = []
+    # 告警数
     alert_count = 0
     alert_data = []
     all_avg = 0
@@ -61,64 +63,70 @@ def scenes_alert(request):
     safe_scene = 0
     will_scene = 0
     danger_scene = 0
+    # 场景的总分值
+    all_score = 0
+    # 场景的错误分值
+    error_score = 0
+    # 多场景时的分数
+    s_score = 0
+    flag = 1
     for i in ps:
+        # 场景id
         sid = model_to_dict(i)['scene']
         # 场景和监控项关联表
         sm = Scene_monitor.objects.filter(scene_id=sid)
         for x in sm:
-            all_score = 100
-            id = model_to_dict(i)['scene']
-            id = str(id)
-            # 计算一个场景下的总分nums
+            x = model_to_dict(x)
+            # 循环item_id，监控项id
+            x_item_id = x["item_id"]
+            # 得分累加，出循环时得到总分
+            scene_score = x["score"]
+            print scene_score
+            all_score = all_score + scene_score
             try:
-                sql = "SELECT sum(score) from tl_scene_monitor where scene_id = '" + id + "' "
-                DATABASES = settings_development.DATABASES['default']
-                db = MySQLdb.connect(host=DATABASES['HOST'], user=DATABASES['USER'], passwd=DATABASES['PASSWORD'],
-                                     db=DATABASES['NAME'], charset="utf8")
-                cursor = db.cursor()
-                cursor.execute(sql)
-                res1 = cursor.fetchall()
-                nums = list(res1)
-                nums = nums[0][0]
                 # 如果总分为0,直接为0
-                thistory = TDGatherHistory.objects.filter(item_id=model_to_dict(x)['item_id'])
-                for hi in thistory:
+                gather_data = TDGatherData.objects.filter(item_id=x_item_id)
+                for hi in gather_data:
                     his = model_to_dict(hi)
                     his['gather_time'] = hi.gather_time
-
                     if str(his['gather_time']).split(' ')[0] == time.strftime("%Y-%m-%d", time.localtime(time.time())):
-                        # 如果当天时间此监控项出错，那么就减去此监控项得权值,如果总分为0那all_score直接为0
+                        # 如果当天时间此监控项出错，
                         if his['gather_error_log'] != None and his['gather_error_log'] != '':
-                            if nums == 0:
-                                all_score = 0
-                            else:
-                                all_score = all_score - model_to_dict(x)['score'] / nums[0]
+                            error_score = error_score + scene_score
+                        # 没有异常，正常
                         else:
                             pass
+
             except Exception as e:
                 return tools.error_result(e)
-            if all_score == 100:
-                safe_scene = safe_scene + 1
-            elif all_score < 100 and all_score > 90:
-                will_scene = will_scene + 1
-            else:
-                danger_scene = danger_scene + 1
-        all_avg = all_avg + all_score
-
+            alert_log = TdAlertLog.objects.filter(item_id=x_item_id)
+            for y in alert_log:
+                alertd = model_to_dict(y)
+                alertd['alert_time'] = y.alert_time
+                if str(alertd['alert_time']).split(' ')[0] == time.strftime("%Y-%m-%d", time.localtime(time.time())):
+                    alert_count = alert_count + 1
+                    alertd['alert_time'] = str(alertd['alert_time'])
+                    alert_data.append(alertd)
+        # 出场景循环
         # 所有场景下得监控项得到的权值分总和/场景数 = 权值平均分 就是从健康度
-        last_score = all_avg / ps.__len__()
-        alert_log = TdAlertLog.objects.filter(item_id=model_to_dict(x)['item_id'])
-        for y in alert_log:
-            alertd = model_to_dict(y)
-            alertd['alert_time'] = y.alert_time
-            if str(alertd['alert_time']).split(' ')[0] == time.strftime("%Y-%m-%d", time.localtime(time.time())):
-                alert_count = alert_count + 1
-                alertd['alert_time'] = str(alertd['alert_time'])
-                alert_data.append(alertd)
+        # 这是一个场景的健康度
+        last_score = int((all_score - error_score)*100 / all_score)
+        if flag > 1:
+            s_score =int((s_score + last_score)/2)
+        else:
+            s_score = last_score
+        flag = flag + 1
+        # 按场景判断
+        if s_score == 100:
+            safe_scene = safe_scene + 1
+        elif s_score < 100 and all_score > 90:
+            will_scene = will_scene + 1
+        else:
+            danger_scene = danger_scene + 1
+
     dic_data = {
         'alert_count': alert_count,  # 告警数
-        'alert_data': alert_data,  # 告警table数据
-        'last_score': last_score,  # 健康度
+        'last_score': s_score,  # 健康度
         'safe_scene': safe_scene,
         'will_scene': will_scene,
         'danger_scene': danger_scene,
