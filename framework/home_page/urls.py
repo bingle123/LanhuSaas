5b39e9a456dacc3e18b640a764cea3fd1f3a837c
zhangmeiqing <1362430696@qq.com>
2019-05-13 15:07:16 +0800

    zhangmeiqing

--- framework/home_page/urls.py
+++ framework/home_page/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns(
     'home_page.views',
     (r'^get_time/$', 'get_time'),  # 获取时间
     (r'^scenes_alert', 'scenes_alert'),  # 场景告警
+    (r'^select_all', 'select_all'),  # 系统运行
     # (r'^get_scene','get_scene'),
 )
